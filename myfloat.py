63721d1985df54e58880e61d2ab877f66fe540d1
dgmartinezg <dgmartinezg@unal.edu.co>
2018-09-27 21:40:53 -0500

    m=x[0] 	n=x[1] 	inter="" 	decil="" 	for i in m: 		inter=inter + str(i) 	for i in n: 		decil=decil+str(i) 	num=inter+"."+decil 	return num
    def ImTupig(x):
    	num=x
    	return num
    
    def convtupl(otro): #Convierte a Tupla
    	t=str(otro)
    	for i in range(len(t)):
    		if t[i]==".":      #si el numero es un numero es un decimal
    			dec=True
    			break
    		else:               #El numero no es un decimal
    			dec=False
    
    	etupl=[]     # tupla que almacena la parte entera
    	dtupl=[]     # tupla que almacena la parte decimal
    	for j in range(0,i):
    		etupl.append(int(t[j]))   #guarda la parte entera
    	if dec==True:
    		for j in range(i+1,len(t)): #guarda la parte decimal
    			dtupl.append(int(t[j]))
    	else:
    		dtupl.append(0)
    		etupl.append(int(t[i]))
    	return etupl,dtupl

--- myfloat.py
+++ myfloat.py
@@ -1,45 +1,95 @@
 class MyFloat:
 
-    def __init__(self):
-        pass
+	def __init__(self):
+		self.n=num
+		self.e=num[0]
+		self.d=num[1]
 
-    def __add__(self):
-        pass
+	def __add__(self):
+		if isinstance(otro,Operacion):  
+			a=self.n
+			b=otro.n
+			c=Suma(self.n,otro.n)
+          
+			return Operacion(c)
+       
+		elif isinstance(otro,(int,float)):
+			tup=Operacion(convtupl(otro))
+			return Operacion(Suma(self.n,tup.n))
+	def __sub__(self):
+		if isinstance(otro,Operacion):
+			a=self.n
+			b=otro.n
+			return Operacion(res(self.n,otro.n))
+			
+		elif isinstance(otro,(int,float)):
+			tup=Operacion(convtupl(otro))
+			return Operacion(res(self.n,tup.n))
 
-    def __sub__(self):
-        pass
+	def __mul__(self):
+		if isinstance(otro,Operacion):
+			a=self.n
+			b=otro.n  
+			return Operacion(mult(a,b))  
+		elif isinstance(otro,(int,float)):
+			tup=Operacion(convtupl(otro))
+			return Operacion(mult(self.n,tup.n))
+				
 
-    def __mul__(self):
-        pass
+	def __div__(self):
+		if isinstance(otro,Operacion):
+			a=self.n
+			b=otro.n     
+			return Operacion(divs(a,b))
+		elif isinstance(otro,(int,float)):
+			tup=Operacion(convtupl(otro))
+			return Operacion(divs(self.n,tup.n))
+	def __radd__(self):
+		return self.__add__(otro)
+	
 
-    def __div__(self):
-        pass
+	def __rsub__(self):
+        
+		ret= self.__sub__(otro) 
+		if ret.e[0]=="+":
+			ret.e[0]="-"
+		else:
+			ret.e[0]="-"
+		ret=Operacion((ret.e,ret.d))
+		return ret
+		
 
-    def __radd__(self):
-        pass
+	def __rmul__(self):
+		return self.__mul__(otro)
+						
+	def __rdiv__(self):
+		if isinstance(otro,(int,float)):
+			tup=Operacion(convtupl(otro))
+			return Operacion(divs(tup.n,self.n))
+			
 
-    def __rsub__(self):
-        pass
+	def __str__(self):
+		t1=self.n
+		return Cadena(t1)
 
-    def __rmul__(self):
-        pass
+	def __repr__(self):
+		return self.__str__()
 
-    def __rdiv__(self):
-        pass
-
-    def __str__(self):
-        pass
-
-    def __repr__(self):
-        pass
-
-    def __eq__(self):
-        pass
-
-    def __ne__(self):
-        pass
+	def __eq__(self):
+		if res(self.n,otro.n)==(["+",0],[0]):
+			return True
+		else:
+			return False
+			
+	def __ne__(self):
+		if res(self.n,otro.n)!=(["+",0],[0]):
+			return True
+		else:
+			return False
+	
+	
 
 if __name__ == "__main__":
     # Escribir aca el codigo para calcular pi. Al finalizar el calculo solo
     # debe imprimir el valor de pi, sin otros textos ni nada
-    pass
+	pass
